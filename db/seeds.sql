d499921ec3b528996ee5dfb4e8f052fed1c03ffd
Matthew Polson <38453024+Bombinos@users.noreply.github.com>
2018-09-15 23:51:30 -0600

    burger

---
+++ db/seeds.sql
@@ -0,0 +1,31 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Blondes Have More Fun-gus Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("I Know Why the Cajun Burger Sings", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Foot Feta-ish Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Hummus a Tune Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Kale Mary Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Final Kraut Down Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Good Night and Good Leek Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("These Collards Don't Run Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cajun Gracefully Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Tarragon in Sixty Seconds Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Salvador Cauliflower Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Chorizo Your Own Adventure Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Rest in Peas Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Olive and Let Die Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Sweet Chili O’Mine Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("I Don't Bay Leaf in Magic Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Girls Just Wanna Have Fennel Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Little Swiss Bunshine", false);
+
+
+
+
+
+
+
+
+
+
+
+
+
